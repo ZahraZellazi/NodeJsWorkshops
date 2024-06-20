d21739b1235e9a9e9407c535d619d8c537be1175
ZahraZellazi <zahra.zellazi@esprit.tn>
2024-06-20 16:44:29 +0100

    updates

--- ateliernode2/server.js
+++ ateliernode2/server.js
@@ -1,9 +1,49 @@
 var http = require ('http')
+var url = require ("url")
+var querystring = require ('querystring')
+var server = http.createServer(function(req, res) {
+    var page = url.parse(req.url).pathname
+    console.log(page)
+    /*res.writeHead(200, {"content-type": "text/html"});
+    res.write(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8"/>
+            <title>Le titre de ma page</title>
+        </head>
+        <body>
+            <p> voici un paragraphe <strong>HTML</strong> !</p>
+        </body>
+        </html>
+        `);*/
+        
+        
+        
+        
+        /*
+        res.writeHead(200, {"content-type": "text/plain"});
+        if(page == '/')
+            res.write('page d\'acceuil')
+        else if (page == '/contact')
+            res.write('page de contact')
+        else if (page == '/affichage/1/user')
+            res.write('afficher l\'utulisateur qui a l\'id 1 !')
+        else {
+            res.write ('404 not found !')
+        }
+        url.parse(req.url).query
 
-var server = http.createServer(function(req,res){
-    res.writeHead(200)
-    res.end('coucou les babies')
-});
+        */
+
+
+
+        //res.write("coucou les babies")
+        res.end();
+})
+
+
+
 server.listen(8888,()=>{
     console.log('server running ')
 })
