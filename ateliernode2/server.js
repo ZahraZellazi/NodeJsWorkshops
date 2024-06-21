9d0ca85029239609b423e0b2fa60268b19d050ce
ZahraZellazi <zahra.zellazi@esprit.tn>
2024-06-21 10:08:09 +0100

    final_update

--- ateliernode2/server.js
+++ ateliernode2/server.js
@@ -1,10 +1,10 @@
 var http = require ('http')
 var url = require ("url")
 var querystring = require ('querystring')
-var server = http.createServer(function(req, res) {
+/*var server = http.createServer(function(req, res) {
     var page = url.parse(req.url).pathname
     console.log(page)
-    /*res.writeHead(200, {"content-type": "text/html"});
+    res.writeHead(200, {"content-type": "text/html"});
     res.write(`
         <!DOCTYPE html>
         <html>
@@ -34,7 +34,7 @@ var server = http.createServer(function(req, res) {
         }
         url.parse(req.url).query
 
-        */
+        
 
 
 
@@ -42,8 +42,43 @@ var server = http.createServer(function(req, res) {
         res.end();
 })
 
+var server = http.createServer(function(req, res) {
+    var params = querystring.parse(url.parse(req.url).query);
+    
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    
+    if ('id' in params && 'login' in params) {
+        res.write('Votre ID est ' + params['id'] + ' et votre login est ' + params['login']);
+    } else {
+        res.write('Veuillez saisir votre ID et login');
+    }
+    
+    res.end();
+});
+*/
+var server = http.createServer(function(req,res) {
 
-
+    var params =querystring.parse(url.parse(req.url).query);
+    res.writeHead(200,{"Content-Type": "text/plain"});
+    if('id' in params && 'login' in params) {
+    res.write('Votre id est ' + params['id'] +
+    'et votre login' +params['login']);
+    }
+    else{
+    res.write('Veuillez saisir votre id et login !');
+    }
+    res.end();
+    });
+const routeLogger = (path) => {
+    if(path=='/')
+        return { status:200,message :"home page !"};
+    else if(path==="/contact"){
+    return { status : 200,message :"contact page !"};
+    }
+    else if (path==="/Affichage/1/user"){
+    return { status : 400,message :"Not Found!"};
+    }
+    }
 server.listen(8888,()=>{
     console.log('server running ')
 })
